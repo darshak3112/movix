6ae7cf709f715ee73e36fab7045e94885eec93dc
darshak kakadiya <90410040+darshak3112@users.noreply.github.com>
2023-04-20 23:47:38 +0530

    header and footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 import "./style.scss";
-import HeroBanner from './heroBanner/HeroBanner';
-
+import HeroBanner from "./heroBanner/HeroBanner";
 
 const Home = () => {
   return (
-    <div className='homePage'>
-      <HeroBanner/>
+    <div className="homePage">
+      <HeroBanner />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
