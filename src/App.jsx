ab369d3ecbbd12481c1dad999c356bcc4179f0f1
darshak kakadiya <90410040+darshak3112@users.noreply.github.com>
2023-04-20 18:28:43 +0530

    redux store and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchDataFromApi } from "./utils/api";
+import { getApiConfiguration } from "./store/homeSlice";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details"
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore"; 
+import PageNotFounf from "./pages/404/PageNotFound";
 
 function App() {
+  const dispatch = useDispatch();
 
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
 
-  return (
-    <div className="App">
+  const apiTesting = async () => {
+    let data = await fetchDataFromApi("/movie/popular");
+    dispatch(getApiConfiguration(data));
+  };
 
-    </div>
-  )
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  return <div className="App">{url?.total_pages}</div>;
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { store } from "../src/store/store";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
